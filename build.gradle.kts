ee05c3f380765f98c7aec49812d0539ab8086a74
Maciej Makowski <maciejm.github@cfiet.net>
2020-04-25 21:47:08 +0100

    Adding protobuf and grpc setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
+import org.gradle.api.internal.HasConvention
+import org.gradle.kotlin.dsl.provider.gradleKotlinDslOf
 
 plugins {
+    idea
+    java
 	id("org.springframework.boot") version "2.2.6.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("com.google.protobuf") version "0.8.12"
 	kotlin("jvm") version "1.3.71"
 	kotlin("plugin.spring") version "1.3.71"
 }
@@ -14,6 +20,12 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 repositories {
 	mavenCentral()
 }
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.VERSION_11
+}
+
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -40,5 +52,26 @@ tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "11"
+	}
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.0.0"
+	}
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java:1.0.0-pre2"
+		}
+	}
+	generateProtoTasks {
+		ofSourceSet("grpc").forEach { task ->
+			task.plugins {
+				id("grpc") {
+					outputSubDir = "grpc_output"
+				}
+			}
+			task.generateDescriptorSet = true
+		}
 	}
 }
