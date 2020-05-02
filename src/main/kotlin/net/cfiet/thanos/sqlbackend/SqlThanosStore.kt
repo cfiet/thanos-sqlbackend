a38ad5d5c70789c0a6e1a546c9c175993b4bdcbb
Maciej Makowski <maciejm.github@cfiet.net>
2020-05-02 21:42:54 +0100

    Docker environment setup for testing

--- src/main/kotlin/net/cfiet/thanos/sqlbackend/SqlThanosStore.kt
+++ src/main/kotlin/net/cfiet/thanos/sqlbackend/SqlThanosStore.kt
@@ -4,11 +4,13 @@ import io.thanos.protobuf.store.Rpc
 import io.thanos.protobuf.store.StoreGrpc
 import org.lognet.springboot.grpc.GRpcService
 import io.grpc.stub.StreamObserver
+import io.thanos.protobuf.prom.Types
 
 @GRpcService
 class SqlThanosStore : StoreGrpc.StoreImplBase() {
     override fun info(request: Rpc.InfoRequest?, responseObserver: StreamObserver<Rpc.InfoResponse>?) {
-        super.info(request, responseObserver)
+        responseObserver!!.onNext(INFO_RESPONSE)
+        responseObserver!!.onCompleted()
     }
 
     override fun labelNames(request: Rpc.LabelNamesRequest?, responseObserver: StreamObserver<Rpc.LabelNamesResponse>?) {
@@ -21,5 +23,20 @@ class SqlThanosStore : StoreGrpc.StoreImplBase() {
 
     override fun series(request: Rpc.SeriesRequest?, responseObserver: StreamObserver<Rpc.SeriesResponse>?) {
         super.series(request, responseObserver)
+    }
+
+
+    companion object {
+        private val INFO_RESPONSE: Rpc.InfoResponse = Rpc.InfoResponse.newBuilder()
+                .setStoreType(Rpc.StoreType.STORE)
+                .addAllLabelSets(listOf(
+                    Rpc.LabelSet.newBuilder()
+                        .addLabels(io.thanos.protobuf.store.Types.Label.newBuilder()
+                            .setName("Hello")
+                            .setValue("World")
+                        )
+                        .build()
+                ))
+                .build()
     }
 }
